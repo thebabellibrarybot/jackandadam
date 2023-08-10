d52331fb2fcd251e1a899eb9f76aa339730dfb28
geringer <hello@geringer.io>
2023-08-10 16:54:13 -0400

    eod

--- server.js
+++ server.js
@@ -3,4 +3,43 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv');
 const mongoose = require('mongoose');
+const Pages = require('./routes/pages.js');
 console.log('starting')
+
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method, req.body, `Server message from ${PORT}`),
+    next()
+})
+
+
+
+
+const URI = process.env.MONGODB_URI;
+mongoose.strict = false;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApiVersion: '1' });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+}   );
+
+
+
+// routes   
+app.use('/pages', Pages);
+// app.use('/books', require('./routes/books.js'));
+
+
+
+
+
+
+
+app.listen(PORT, () => {    
+    console.log(`Server is running on port: ${PORT}`);
+} );
---
+++ src/routes/books.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const Page = require('../controllers/pageController');
+
+// read
+
+router.get('/', Page.index);
+router.post('/', Page.create);
+router.get(':id', Page.show);
+router.push('/', Page.update);
+router.delete('/', Page.delete);
+
+module.exports = router;
+
